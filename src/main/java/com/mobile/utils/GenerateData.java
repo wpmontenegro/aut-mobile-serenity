10f4e3f30eaa2d66661f2da1c740debd6b3db2e3
William Montenegro <williammontenegro4d@gmail.com>
2025-01-29 16:06:14 -0500

    Add mask logic

--- src/main/java/com/mobile/utils/GenerateData.java
+++ src/main/java/com/mobile/utils/GenerateData.java
@@ -3,7 +3,19 @@ package com.mobile.utils;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GenerateData {
+    private static final String REGEX_MASK_BEHIND = "(?<=.{%s}).";
+    private static final String REGEX_MASK_AHEAD = ".(?=.{%s})";
+    private static final String MASK_CHARACTER = "*";
+
     public static int randomInteger(int minInclusive, int maxExclusive) {
         return ThreadLocalRandom.current().nextInt(minInclusive, maxExclusive);
+    }
+
+    public static String maskLookAhead(String rawData, int length) {
+        return rawData.replaceAll(String.format(REGEX_MASK_AHEAD, length), MASK_CHARACTER);
+    }
+
+    public static String maskLookBehind(String rawData, int length) {
+        return rawData.replaceAll(String.format(REGEX_MASK_BEHIND, length), MASK_CHARACTER);
     }
 }
